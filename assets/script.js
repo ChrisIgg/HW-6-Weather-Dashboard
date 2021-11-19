01a89dcd013118b5874d2239d950b9124a121574
Chris Insignares <insignaresc115@gmail.com>
2021-11-19 09:18:29 -0500

    update css with bulma and creaeted input link functionality to set to local storage

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// fetch("https://api.github.com/repos/nodejs/node/issues?per_page=5", {
+//   method: "GET", //GET is the default.
+//   credentials: "same-origin", // include, *same-origin, omit
+//   redirect: "follow", // manual, *follow, error
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+var openWeatherApiKey = "76cd20c8e01161e513e19b0cb3d25fa7";
+
+const button = document.querySelector("#searchBtn");
+
+button.addEventListener("click", (event) => {
+  button.textContent = `Click count: ${event.detail}`;
+  setLocation();
+});
+
+function setLocation() {
+  var cityLocation = document.getElementById("city-select");
+  localStorage.setItem("city", cityLocation.value);
+  console.log(cityLocation.value);
+}
+
+const cityUser = localStorage.getItem("city")
+
+mini project
+$cityUser
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
