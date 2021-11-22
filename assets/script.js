5b855dd7fab5f9c3af81a6c135fd8381bdad762d
Chris Insignares <insignaresc115@gmail.com>
2021-11-21 21:15:47 -0500

    update with latest

--- assets/script.js
+++ assets/script.js
@@ -9,14 +9,34 @@
 //   .then(function (data) {
 //     console.log(data);
 //   });
-var openWeatherApiKey = "76cd20c8e01161e513e19b0cb3d25fa7";
+var apiKey = "76cd20c8e01161e513e19b0cb3d25fa7";
+
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  cityUser +
+  "&appid=" +
+  apiKey;
+
+var requestUrl = "..."
 
 const button = document.querySelector("#searchBtn");
 
 button.addEventListener("click", (event) => {
   button.textContent = `Click count: ${event.detail}`;
   setLocation();
+  fetch(queryURL);
+  console.log(queryURL);
 });
+
+function getApi() {
+  fetch(requestUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+}
 
 function setLocation() {
   var cityLocation = document.getElementById("city-select");
